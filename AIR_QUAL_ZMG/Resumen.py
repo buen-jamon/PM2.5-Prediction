ab7f5419a2340bef82c7671784d20b147b4ce918
Benjamino <xdcoyul@gmail.com>
2026-07-01 17:43:47 -0600

    Refactor ResumenDf function parameter name
    Updated the function to use the parameter name 'DataFrame' instead of 'raw18'.

--- AIR_QUAL_ZMG/Resumen.py
+++ AIR_QUAL_ZMG/Resumen.py
@@ -6,7 +6,7 @@ pd.set_option('display.max_rows', 50)
 
 #Created a function to automate some parts of our cleaning process
 def ResumenDf(DataFrame):
-    print(raw18.head()) #Shows the first five rows of our dataframe
-    print(raw18.dtypes) #Print the type of data of each column
-    print((raw18.isna().mean()*100).sort_values(ascending=False)) #.isna retrieves a array where if theres a NaN the value is filled with 'True' otherwise is 'False' both are equivalent to 0 n' 1 
+    print(DataFrame.head()) #Shows the first five rows of our dataframe
+    print(DataFrame.dtypes) #Print the type of data of each column
+    print((DataFrame.isna().mean()*100).sort_values(ascending=False)) #.isna retrieves a array where if theres a NaN the value is filled with 'True' otherwise is 'False' both are equivalent to 0 n' 1 
     #so with .mean() it adds each element and divides by the number of values * 100 transforms into percentage and .sort_values(ascending=False) ordenates max-min or min-max if we set it as 'True'
